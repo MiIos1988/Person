91cb66e5ba548f26f1b7b7904d663d51312d3b0d
Milos1988 <vojvoda1988@gmail.com>
2023-09-08 00:30:03 +0200

    Corrected code

--- src/component/PersonList.js
+++ src/component/PersonList.js
@@ -2,13 +2,13 @@ import React from "react";
 import Person from "./Person";
 
 function PersonList({ data, deletePerson }) {
-  const listOfPerson = data.map((person) => {
-    return <Person data={person} key={person.id} deletePerson={deletePerson} />;
-  });
-
-  return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>{listOfPerson}</div>
-  );
+  return <div style={{ display: "flex", flexWrap: "wrap" }}>
+    {
+       data.map((person) => {
+        return <Person data={person} key={person.id} deletePerson={deletePerson} />;
+      })
+    }
+  </div>
 }
 
 export default PersonList;
